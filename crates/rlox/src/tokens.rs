86267f23a1d0076ef9e806570873cf4f608a1076
Amanjeev Sethi <aj@amanjeev.com>
2024-12-13 13:22:30 -0500

    Tokens and types

--- crates/rlox/src/tokens.rs
+++ crates/rlox/src/tokens.rs
@@ -1,3 +1,6 @@
+use std::fmt::{Display, Formatter};
+
+#[derive(Debug)]
 enum TokenType {
     // Single-character tokens.
     LeftParen,
@@ -46,4 +49,38 @@ enum TokenType {
     While,
 
     Eof,
+}
+
+impl Display for TokenType {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+#[derive(Debug)]
+struct Literal;
+
+#[derive(Debug)]
+struct Token {
+    ttype: TokenType,
+    lexeme: String,
+    line: usize,
+    literal: Literal,
+}
+
+impl Token {
+    fn new(ttype: TokenType, lexeme: String, line: usize, literal: Literal) -> Self {
+        Token {
+            ttype,
+            lexeme,
+            line,
+            literal,
+        }
+    }
+}
+
+impl Display for Token {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?} {} {:?}", self.ttype, self.lexeme, self.literal)
+    }
 }
